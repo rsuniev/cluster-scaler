8cc0b4c9b0e5949886e63249a9572d335389f246
Rustem Suniev <rustem.suniev@6point6.co.uk>
2017-06-07 18:43:25 +0100

    Move to 1 script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,10 +3,27 @@ aws configure set default.region us-west-1
 
 if [ "$ACTION_TYPE" == "SCALE_DOWN" ]
 then
-  source scripts/scale_down.sh
+
+  echo "Scaling compute ASG to 1"
+  aws-retry.sh aws autoscaling set-desired-capacity --auto-scaling-group-name=$ASG  --desired-capacity 1
+  echo "Done"
+
 elif [ "$ACTION_TYPE" == "SCALE_UP" ]
 then
-  source scripts/scale_up.sh
+
+  current_capacity=$(aws-retry.sh aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name=${ASG}| jq .AutoScalingGroups[0].DesiredCapacity)
+  while [ $current_capacity -lt $DESIRED_CAPACITY ]
+  do
+    new_capacity=`expr $current_capacity + 1`
+    echo "Setting a new cluster capacity to: $new_capacity"
+    aws-retry.sh aws autoscaling set-desired-capacity --auto-scaling-group-name=$ASG  --desired-capacity $new_capacity
+    sleep 30s
+    current_capacity=$(aws-retry.sh aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name=${ASG}| jq .AutoScalingGroups[0].DesiredCapacity)
+  done
+  echo "Done"
+
 else
+
   echo "Unknown action type: $ACTION_TYPE"
+
 fi
--- scripts/scale_down.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-echo "Scaling compute ASG to 1"
-aws-retry.sh aws autoscaling set-desired-capacity --auto-scaling-group-name=$ASG  --desired-capacity 1
-echo "Done"
